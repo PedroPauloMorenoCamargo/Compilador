b0b12d05cd7e404d433eb6b435df0d710735d887
PedroPauloMorenoCamargo <ppmcamargo@gmail.com>
2024-08-05 18:51:37 -0300

    a

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+
+argumento = ''.join(sys.argv[1:])
+argumento = argumento.replace(" ", "")
+
+numero_final = 0
+numero_atual = ""
+operador_atual = "+"
+
+for caracter in argumento:
+    if caracter.isnumeric():
+        numero_atual += caracter
+    else:
+        if numero_atual:
+            if operador_atual == "+":
+                numero_final += int(numero_atual)
+            elif operador_atual == "-":
+                numero_final -= int(numero_atual)
+            numero_atual = ""
+        operador_atual = caracter
+
+if numero_atual:
+    if operador_atual == "+":
+        numero_final += int(numero_atual)
+    elif operador_atual == "-":
+        numero_final -= int(numero_atual)
+
+print(numero_final)
