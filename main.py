daf14fa712a824cb9a9b5ec5f9dba5f22097e7fb
PedroPauloMorenoCamargo <ppmcamargo@gmail.com>
2024-08-05 19:05:37 -0300

    a

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ operador_atual = "+"
 for caracter in argumento:
     if caracter.isnumeric():
         numero_atual += caracter
-    else:
+    elif caracter == "+" or caracter == "-":
         if numero_atual:
             if operador_atual == "+":
                 numero_final += int(numero_atual)
             elif operador_atual == "-":
                 numero_final -= int(numero_atual)
-            numero_atual = ""
+        numero_atual = ""
         operador_atual = caracter
 
 if numero_atual:
