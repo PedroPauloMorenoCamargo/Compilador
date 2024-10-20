38af667f49437f378c4786d04772497b64fed20a
Pedro Paulo Moreno Camargo <ppmcamargo@gmail.com>
2024-10-20 14:41:09 -0300

    b

--- teste.asm
+++ teste.asm
@@ -1,52 +1,4 @@
-from classes import *
-import sys
-from arvore import *
 
-# Verifica se o arquivo foi passado como argumento
-if len(sys.argv) < 2:
-    print("Uso: python3 parser.py <arquivo.c>")
-    sys.exit(1)
-
-# Determina o nome do arquivo
-filename = sys.argv[1]
-
-#troca ".c" por ".asm"
-
-file_out = filename.replace(".c", ".asm")
-
-print(file_out)
-
-# Lê o código do arquivo
-with open(filename, 'r') as file:
-    code = file.read()
-
-# Filtra o código
-filtered_code = PrePro.filter(code)
-
-# Cria o parser
-parser = Parser()
-
-# Cria a árvore sintática
-ast = parser.run(filtered_code)
-
-# Cria a tabela de símbolos
-symbol_table = SymbolTable()
-
-# Cria uma lista para armazenar o código assembly gerado
-assembly_code = []
-
-# Create the LabelGenerator instance
-label_generator = LabelGenerator()
-
-# Avalia a árvore sintática e gera o código assembly
-if ast:
-    # Pass all the necessary arguments (assembly_code, symbol_table, label_counter)
-    ast.Evaluate(assembly_code, symbol_table, label_generator)
-
-
-
-# Assembly code header
-assembly_header = """
 SECTION .data
     ten     dd 10       ; Constant value 10
 
@@ -59,10 +11,21 @@ SECTION .text
 main:
     PUSH EBP            ; Set up stack frame
     MOV EBP, ESP
-"""
+    PUSH DWORD 0 ; allocate space for x
+    MOV EBX, 0 ; default initialization for x
+    MOV [EBP-4], EBX ; store default value in x
+    MOV EBX, 10 ; Evaluate IntVal
+    MOV [EBP-4], EBX ; store the value in x
+    PUSH DWORD 0 ; allocate space for y
+    MOV EBX, 0 ; default initialization for y
+    MOV [EBP-8], EBX ; store default value in y
+    PUSH EBX
+    MOV EBX, 5 ; Evaluate IntVal
+    POP EAX
+    SUB EAX, EBX
+    MOV EBX, EAX
+    MOV [EBP-8], EBX ; store the value in y
 
-# Assembly code footer
-assembly_footer = """
     MOV ESP, EBP        ; Clean up stack frame
     POP EBP
     MOV EAX, 1          ; sys_exit
@@ -119,17 +82,3 @@ print_end_loop:
     MOV ESP, EBP
     POP EBP
     RET
-"""
-
-# Função para escrever o código assembly gerado em um arquivo
-def write_assembly_to_file(assembly_code, output_filename):
-    # Write the assembly code to the file
-    with open(output_filename, 'w') as asm_file:
-        asm_file.write(assembly_header)
-        for line in assembly_code:
-            asm_file.write("    "+line + '\n')
-        asm_file.write(assembly_footer)
-
-# Escreve o código assembly gerado no arquivo
-output_filename = file_out
-write_assembly_to_file(assembly_code, output_filename)
